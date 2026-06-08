3713122856f3c0dd827ab14d2d0a06c82eea3c2f
jrodriguez54636-ops <jrodriguez54636@ucq.edu.mx>
2026-06-08 03:39:06 -0600

    el doxygen
    los archivos que creo el doxygen

---
+++ html/search/pages_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['fabrica_20de_20peluches_20proyecto_0',['fabrica-de-peluches-proyecto',['../md__r_e_a_d_m_e.html',1,'']]]
+];
---
+++ html/search/pages_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['peluches_20proyecto_0',['fabrica-de-peluches-proyecto',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['proyecto_1',['fabrica-de-peluches-proyecto',['../md__r_e_a_d_m_e.html',1,'']]]
+];
